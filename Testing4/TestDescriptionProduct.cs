477dd04dab84300b7e73be67eaca0f0129bcdaf7
Jayden Broadbelt <P2716294@my365.dmu.ac.uk>
2024-06-03 21:05:04 +0100

    Added more tests

---
+++ Testing4/TestDescriptionProduct.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class TestDescriptionProduct
+    {
+        [TestMethod]
+        public void DescriptionProductOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AProduct = new clsProduct();
+            //create some test data to assign to the product
+            string TestData = "Describe the Product";
+            //assign the data to the product
+            AProduct.Description = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AProduct.Description, TestData);
+        }
+    }
+}
---
+++ Testing4/TestNameProduct.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class TestNameProduct
+    {
+        [TestMethod]
+        public void NameProductOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AProduct = new clsProduct();
+            //create some test data to assign to the product
+            string TestData = "Name of Product";
+            //assign the data to the product
+            AProduct.Name = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AProduct.Name, TestData);
+        }
+    }
+}
